0eebe53f3d4ee7a39b7034eb5da60a892415b401
karenioane-tech <karenmwaniki1@gmail.com>
2026-02-18 21:02:14 +0300

    add karen.py,students_details.py

---
+++ karen.py
@@ -0,0 +1,4 @@
+fav_sport="swimming"
+fav_hobby="travelholic"
+fav_color="grey"
+fav_perfume="lattafa"
---
+++ students_details.py
@@ -0,0 +1,6 @@
+from karen import fav_sport, fav_hobby, fav_color, fav_perfume
+
+print(f"Favorite sport: {fav_sport}")
+print(f"Favorite hobby: {fav_hobby}")
+print(f"Favorite color: {fav_color}")
+print(f"Favorite perfume: {fav_perfume}")
