9400500549c53369a407efa798368c248d42b8d9
Reenie_. <mollymbugua123456@gmail.com>
2026-02-23 08:36:59 +0300

    Create Students_details 1.py with personal info
    Added personal details and favorite attributes.

---
+++ Students_details 1.py
@@ -0,0 +1,9 @@
+#Maureen Mbugua
+#18/02/2026 
+
+
+print(f"my favourite hobbyis {hobby}")
+print(f"my favourite food is {food}")
+print(f"my favourite sport is {sport}")
+print(f"my values are {values}")
+print(f"my trait is {trait}")
